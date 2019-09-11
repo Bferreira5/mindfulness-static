a50aaedc91a58b81118d971c9cc78a26d54c1636
Benjamin Ferreira <benjaminferreira@Benjamins-MacBook-Pro.local>
2019-09-11 11:43:26 -0400

    Update for deployment

--- javascript/main.js
+++ javascript/main.js
@@ -22,6 +22,21 @@ page = function page(name) {
 };
 // add a class of 'intro' to the first paragraph
 // do this on every page apart from the blog page
-if (!page("blog")) {
-  document.querySelector(".content > p").classList.add("intro");
-}
+
+const words = document.getElementsByClassName('words');
+const highlight = document.getElementById('highlighted');
+
+
+Array.prototype.forEach.call(words, (element) => {
+  console.log(element);
+  element.innerHTML = element.innerHTML.replace(/(^|<\/?[^>]+>|\s+)([^\s<]+)/g, '$1<span class="word">$2</span>');
+
+  element.addEventListener('click', function(eventAction) {
+    if (eventAction.target.classList.contains('word')) {
+      eventAction.target.classList.toggle('highlighted');
+      highlighted.innerHTML = 'Highlighted: ' + element.querySelectorAll('.highlighted').length;
+    }
+  }, true);
+});
+
+
