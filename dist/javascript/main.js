4534b36715fb5d9cec2ccbded1eadaa046507553
Benjamin Ferreira <benjaminferreira@Benjamins-MacBook-Pro.local>
2019-09-11 12:04:24 -0400

    Clearing JS errors

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -1,10 +1,6 @@
 // Config
 // Selectors
 // store selectors for reference so we only call them once
-var $body = document.querySelector('body');
-var $header = document.getElementById('#header');
-var $nav = document.getElementById('#nav');
-var $footer = document.getElementById('#footer');
 var words = document.getElementsByClassName('words');
 var highlight = document.getElementById('highlighted');
 Array.prototype.forEach.call(words, function (element) {
@@ -17,23 +13,4 @@ Array.prototype.forEach.call(words, function (element) {
     }
   }, true);
 });
-// Helpers
-var
-/**
-* @description Test if the body id is something
-* @param  		{string}
-* @return 		{bool}
-*
-*/
-page = function page(name) {
-  if (!name) {
-    return $body.getAttribute('id');
-  }
 
-  return $body.getAttribute('id') == name;
-};
-// add a class of 'intro' to the first paragraph
-// do this on every page apart from the blog page
-if (!page("blog")) {
-  document.querySelector(".content > p").classList.add("intro");
-}
--- resources/js/utilities/_config.js
+++ resources/js/utilities/_config.js
@@ -3,13 +3,6 @@
 
 // Selectors
 // store selectors for reference so we only call them once
-const $body = document.querySelector('body')
-
-const $header = document.getElementById('#header')
-
-const $nav = document.getElementById('#nav')
-
-const $footer = document.getElementById('#footer')
 
 
 const words = document.getElementsByClassName('words');
